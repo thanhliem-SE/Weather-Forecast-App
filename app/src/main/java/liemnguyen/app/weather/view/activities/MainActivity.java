d228a4381d20cb12a528d105fea4bfeb44248bc6
thanhliem-SE <thanhliem2n@gmail.com>
2021-10-03 12:07:30 +0700

    init package project according to the MVP architecture

---
+++ app/src/main/java/liemnguyen/app/weather/view/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package liemnguyen.app.weather.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import liemnguyen.app.weather.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
